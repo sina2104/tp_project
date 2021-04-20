f31a3575aff2b72db7e7e08a8c8f8821b920a040
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-04-20 21:50:43 +0300

    Create FieldObject.py

---
+++ tp_project/FieldObject.py
@@ -0,0 +1,16 @@
+class FieldObject():
+    def __init__(self):
+        self.name = ''
+        self.x = 0
+        self.y = 0
+        self.list_of_fruits = ['L', 'W']
+
+    def create(self, name):
+        import random
+        if name == "fruit":
+            self.name = random.choice(self.list_of_fruits)
+        else:
+            self.name = name
+        self.x = random.randint(-1, 14)
+        self.y = random.randint(-1, 14)
+        return self.name, self.x, self.y
