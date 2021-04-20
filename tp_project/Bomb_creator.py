fc49d456ac7146ef5674750493cf2fab281a0fcc
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-04-20 21:52:39 +0300

    Update Bomb_creator.py

--- tp_project/Bomb_creator.py
+++ tp_project/Bomb_creator.py
@@ -1,34 +1,20 @@
-from Creator import Creator, field
+from Creator import Creator
+from Globals import field
+
 
 class Bomb_creator(Creator):
-
     def __init__(self):
-
-        self.name = ''
-
-        self.x = 0
-
-        self.y = 0
-
         super().__init__()
-
-
 
     def create_Bomb(self):
 
-        import random
-
-        name = random.random()
-
-        if name > 0.5:
-
-            name = "B"
-
-            x = random.randint(-1, 14)
-
-            y = random.randint(-1, 14)
-
-            Creator.create_obj(self, x, y, name)
-
+        obj = FieldObject()
+        name, x, y = obj.create('B')
+        bomb = Bomb()
+        bomb.name = 'B'
+        bomb.x = x
+        bomb.y = y
+        Creator.create_obj(self, x, y, name)
+        return bomb
 
 
