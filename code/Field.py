105c2a3af1384c3ff39218840f3d5e38298d9a43
Sasha-nagibator <79227642+Sasha-nagibator@users.noreply.github.com>
2021-05-05 20:19:45 +0300

    Corrected

--- code/Field.py
+++ code/Field.py
@@ -62,7 +62,7 @@ class Field:
             x = int(input())
             y = int(input())
             self.last_point = (x, y)
-            # self.last_point = (0, 0)
+
             if self.check_bomb():
                 return True
             if not self.check_fruit():
@@ -73,39 +73,43 @@ class Field:
             self.move = self.move + 1 - your_move_again
 
     def check(self):
+        field_len = 14
+
         minimum = min(self.last_point)
 
         start = (self.last_point[0] - minimum, self.last_point[1] - minimum)
 
         diagonal_arr1 = [self.field[start[0] + i][start[1] + i] for i in range(15 - max(start))]
-
-        if self.last_point[0] + self.last_point[1] < 14:
-
+        # diagonal_arr1 - массив элементов в поле, которые стоят по диагонали
+        # (по направлению сверху слева - снизу справа), переменная создается для
+        # проверки не закончилась ли игра. Если в этом массиве 5 единиц или 5 двоек подряд -
+        # игра закончилась
+        if self.last_point[0] + self.last_point[1] < field_len:
             start = (0, self.last_point[0] + self.last_point[1])
 
-        if self.last_point[0] + self.last_point[1] > 14:
+        if self.last_point[0] + self.last_point[1] > field_len:
+            start = ((self.last_point[0] + self.last_point[1]) % field_len, field_len)
 
-            start = ((self.last_point[0] + self.last_point[1]) % 14, 14)
-
-        if self.last_point[0] + self.last_point[1] == 14:
-
-            start = (0, 14)
+        if self.last_point[0] + self.last_point[1] == field_len:
+            start = (0, field_len)
 
         diagonal_arr2 = [self.field[start[0] + i][start[1] - i] for i in range(start[1] - start[0] + 1)]
+        # аналогично diagonal_arr1, только направление сверху справа - снизу слева
 
-        vertical_arr = [self.field[self.last_point[0]][i] for i in range(15)]
+        vertical_arr = [self.field[self.last_point[0]][i] for i in range(field_len + 1)]
+        # аналогично diagonal_arr1, только направление по вертикали
 
-        horizontal_arr = [self.field[i][self.last_point[1]] for i in range(15)]
+        horizontal_arr = [self.field[i][self.last_point[1]] for i in range(field_len + 1)]
+        # аналогично diagonal_arr1, только направление по горизонтали
 
         all_arr = diagonal_arr1 + [0] + diagonal_arr2 + [0] + vertical_arr + [0] + horizontal_arr
+        # все массивы вместе, в которых ищем [1, 1, 1, 1, 1] или [2, 2, 2, 2, 2]. Если находим - игра заканчивается
 
         for i in range(len(all_arr) - self.win_strike_lenth):
 
-            if all_arr[i: i + self.win_strike_lenth] == [1, 1, 1, 1, 1]:
-
+            if all_arr[i: i + self.win_strike_lenth] == [1, 1, 1, 1, 1] or \
+                    all_arr[i: i + self.win_strike_lenth] == [2, 2, 2, 2, 2]:
                 return True
-
-        # print(1)
 
         return False
 
@@ -122,7 +126,8 @@ class Field:
             return False
 
     def check_fruit(self):
-        if self.field[self.last_point[0]][self.last_point[1]] == 'W' or self.field[self.last_point[0]][self.last_point[1]] == 'L':
+        if self.field[self.last_point[0]][self.last_point[1]] == 'W' or \
+           self.field[self.last_point[0]][self.last_point[1]] == 'L':
             import random
             your_turn_again = random.random()
             if your_turn_again > 0.5:
